29df9b8766853704c1a0e89db9e2cf8bfe724bbb
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-04-05 11:28:02 +0530

    Add files via upload

---
+++ string(list)-14.py
@@ -0,0 +1,5 @@
+# Inserting element at a specified index
+L1=[1,'a',2,'b',3,'c']                       # L1 for store the value
+print(L1)                                    # print L1
+L1.insert(2,'poorna')         # insert 2 in pooorna
+print(L1)                                    # execute L1 for changes
